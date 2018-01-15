c0c1cb5a22b4404ee86b854eb471ac2af5255353
kimserey <kimserey.lam@groomgy.com>
2018-01-15 21:21:53 +0800

    release script

---
+++ release.cmd
@@ -0,0 +1,11 @@
+REM Checkout release branch
+git checkout release
+
+REM Merge master to release
+git merge master
+
+REM Pushes on remote github, local/release to github/master 
+git push github release:master
+
+REM Checkout master branch
+git checkout master
